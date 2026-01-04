9b185dbd19cda987f1e16eec0fbb9f74aa34b10c
RaysFX <103001446+RaysFX96@users.noreply.github.com>
2026-01-04 20:32:25 +0100

    Update script.js
    mobile fix

--- script.js
+++ script.js
@@ -957,6 +957,25 @@ onboardingInput.addEventListener("input", () =>
   autoResizeTextarea(onboardingInput)
 );
 
+/* ------------------------------
+   SIDEBAR MOBILE TOGGLE
+------------------------------ */
+
+const mobileMenuBtn = document.getElementById("mobileMenuBtn");
+const sidebar = document.querySelector(".sidebar");
+const sidebarOverlay = document.getElementById("sidebarOverlay");
+
+mobileMenuBtn.addEventListener("click", () => {
+  sidebar.classList.add("open");
+  sidebarOverlay.classList.add("show");
+});
+
+sidebarOverlay.addEventListener("click", () => {
+  sidebar.classList.remove("open");
+  sidebarOverlay.classList.remove("show");
+});
+
+
 /* ---------------------------------------------------
    INIT
 --------------------------------------------------- */
